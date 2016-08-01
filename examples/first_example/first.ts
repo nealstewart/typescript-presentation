d7a5949820492dd9de6c6e5d468a1c541cf3d4d1
Neal Stewart <nes@tradeshift.com>
2016-08-01 19:50:48 +0200

    Delete some stuff

--- examples/first_example/first.ts
+++
@@ -1,7 +0,0 @@
-var myObj = {
-    addThree: function (a: number) { return a + 3; }
-};
-
-var numToAdd = 3;
-
-console.log(myObj.addThree(numToAdd));
--- examples/function/function.js
+++
@@ -1,7 +0,0 @@
-function createAdder(firstNumber) {
-    return function (secondNumber) {
-        return firstNumber + secondNumber;
-    };
-}
-var addThree = createAdder(3);
-console.log(addThree(3));
