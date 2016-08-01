3f0f7edfe2acfab242aa6b57c83b69f58b3c8f74
Neal Stewart <nes@tradeshift.com>
2016-08-01 20:18:05 +0200

    First step of conversion

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,11 @@
 /// <reference path="../typings/index.d.ts" />
 var express = require('express');
 var app = express();
+
 var firstController = require('./first_controller');
 var secondController = require('./second_controller');
+
 app.get('/', firstController.index);
 app.get('/other', secondController.other);
+
 app.listen(3000);
